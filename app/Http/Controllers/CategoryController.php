b08362702d493f72139700d16d75184ac4edee41
mpak59rus <mpak59rus@list.ru>
2020-04-24 19:16:05 +0300

    Delete collections

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\CategoriesCollection;
 use App\Models\Category;
 use App\Services\CacheService;
 
@@ -11,17 +10,16 @@ class CategoryController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return CategoriesCollection
      */
     public function index()
     {
         $dataCacheKey = env('CATEGORIES_CACHE_KEY', 'categories_cache_key') . '_data';
 
         return CacheService::remember($dataCacheKey, 24 * 60 * 60, function () {
-            return new CategoriesCollection(Category::all(Category::CATEGORY_FIELDS)
+            return Category::all(Category::CATEGORY_FIELDS)
                 ->filter(function (Category $category) {
                     return $category->products()->count() > 0 ? true : false;
-                })->sortBy('sort')->values());
+                })->sortBy('sort')->values();
         });
     }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,26 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\ProductsCollection;
 use App\Models\Product;
 use App\Services\CacheService;
-use Illuminate\Http\Response;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return ProductsCollection
      */
     public function index()
     {
         $dataCacheKey = env('PRODUCTS_CACHE_KEY', 'products_cache_key') . '_data';
 
         return CacheService::remember($dataCacheKey, 24*60*60, function () {
-            return new ProductsCollection(
-                Product::all(Product::PRODUCT_FIELDS)->sortBy('sort')->values()
-            );
+            return Product::all(Product::PRODUCT_FIELDS)->sortBy('sort')->values();
         });
     }
 }
